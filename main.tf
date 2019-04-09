e5a1004916bc3f98f3008a826856a86c2242af52
Jonathan Wright <jon@than.io>
2019-04-09 17:41:13 +0100

    Fix duplicate SG rule

--- main.tf
+++ main.tf
@@ -212,7 +212,7 @@ module "sg_alb_nginx" {
       source_security_group_id = "${module.sg_ec2_nginx.this_security_group_id}"
     },
     {
-      rule                     = "http-80-tcp"
+      rule                     = "https-443-tcp"
       source_security_group_id = "${module.sg_ec2_nginx.this_security_group_id}"
     },
   ]
